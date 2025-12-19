a27cb3bb9120db90f4a401655c8a815c4a772593
sarfaz <sarfazsarfaz646@gmail.com>
2025-12-19 13:48:14 +0530

    19/12/25

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 const express = require('express')
-const morgan = require('morgan')
-
 const app = express();
 
+const users = [
+  {
+    id: 1,
+    name : 'alex',
+    email : 'alex@gmail.com',
+    password :'123'
+  },
+  {
+    id: 2,
+    name : 'joe',
+    email : 'joe@gmail.com',
+    password :'456'
+  }
+]
+
 app.use(express.json());
-
-app.use(morgan("tiny"))
-
+  
 app.get("/", (req, res) => {
   return res.send("Hello World")
 });
 
-app.use("/users", require("./routers/users.router"));
+app.post("/login", (req, res) => {
+  const {email , password } = req.body;
+  const user = users.find((user) => user.email === email && user.password === password);
+  if(!user) {
+    return res.status(401).json({message:"Invalid username or password"})
+  }
+  return res.send("Login Success")
+});
+
+app.get("/profile", (req, res) => {
+  return res.send("profile");
+})
 
 app.listen(3000, () => {
   console.log("server is running on port 3000")
